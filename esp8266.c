9d6a7a3e75e389f3433187930dd8f2024b88c2a5
ZechariahZheng <1173478308@qq.com>
2018-05-22 16:35:07 +0800

    编译是否通过：是 进度：完成串口的模拟，并验证了串口的写功能是有效的

--- esp8266.c
+++ esp8266.c
@@ -16,7 +16,6 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/init.h>
-#include <linux/delay.h>
 #include <linux/irq.h>
 
 #include <asm/uaccess.h>
@@ -25,6 +24,9 @@
 //#include <asm/arch/regs-gpio.h>
 //#include <asm/hardware.h>
 #include <linux/unistd.h>
+#include <linux/slab.h>
+
+#define DELAYTIME 104       //单位us，波特率为9600时，每位的延时时间
 
 static struct class *esp8266_class;
 static struct class_device *esp8266_class_dev; 
@@ -32,10 +34,65 @@ static struct class_device *esp8266_class_dev;
 volatile unsigned long *gpbcon = NULL;
 volatile unsigned long *gpbdat = NULL;
 
+/*串口写函数*/
+static void uart_write(unsigned char data)
+{
+    int i = 8;
+    
+    /*发送起始位*/
+    *gpbdat &=~(1<<8);
+    udelay(DELAYTIME);
+    
+    /*发送8位数据位*/
+    unsigned char tmp;
+    while (i--)
+    {
+        tmp = data & 0x01;
+        /*先发送低位*/
+        if (tmp == 0x01)
+        {
+            *gpbdat |=(1<<8);
+        }
+        else
+        {
+            *gpbdat &=~(1<<8);
+        }
+        udelay(DELAYTIME);
+        data = data >> 1;
+    }
+    /*发送校验位*/
+    *gpbdat |= (1<<8);
+    udelay(DELAYTIME);
+}
+
+/*串口读函数*/
+static unsigned char uart_read(void)
+{
+    unsigned char result;
+    int i = 8;
+    unsigned char port_val = 0x00;
+    
+    /*等过起始位*/
+    udelay(DELAYTIME);
+    while (i--)
+    {
+        result >>= 1;
+        port_val=readw(gpbdat);
+        if((port_val|0x7f) == 0xff)
+        {
+            result |= 0x80;
+        }
+        udelay(DELAYTIME);
+    }
+    /*等过结束位*/
+    udelay(DELAYTIME);
+    return result;
+}
+
 static void esp8266_GPIO_CFG(void)
 {
-    /*配置GPIO引脚*/
-    /* 配置GPB7为串口输入引脚，GPB8为串口输出引脚*/
+    /*配置GPIO引脚，模拟串口*/
+    /* 配置GPB7为串口输入引脚，GPB8为串口输出引脚(相对于arm板)*/
     *gpbcon &= ~(0x3<<(7*2));
     *gpbcon |= (0x1<<(8*2));
 }
@@ -48,6 +105,27 @@ static int esp8266_open(struct inode *inode, struct file *file)
 
 static ssize_t esp8266_write(struct file *file, const char __user *buf, size_t count, loff_t * ppos)
 {
+    unsigned char *tmp = (unsigned char *)kmalloc(count, GFP_KERNEL);
+    int i = 0;
+    int error;
+    
+    if (tmp == NULL)
+    {
+        printk("kmalloc failed!\n");
+        return -1;
+    }
+    for (i = 0; i < count; i++)
+    {
+        error = copy_from_user(&tmp[i],buf,1);
+        if (error < 0)
+            printk("copy from user failed!");
+        else
+        {
+            printk("tmp[i] is: %d\n", tmp[i]);
+            uart_write(tmp[i]);
+        }
+    }
+    kfree(tmp);
     return 0;
 }
 
